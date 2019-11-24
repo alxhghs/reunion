c3379fac607d2c33bba9293b9d61b62ab521e0e0
alexhughesef <alex.hughes@ef.com>
2019-11-24 08:42:23 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const Home = () => {
+    const [data, setData] = useState([]);
+
+    return (
+        <Head>
+            <title>Reunion</title>
+        </Head>
+    )
+};
+
+export default Home;
